ed6e8e4a55517c7d0d49ca4a34220c83d2d36e3e
Enrique <enriquedaviacosta18@gmail.com>
2026-06-01 10:59:58 -0300

    Actualizar enlace de TikTok a @dani.cash90

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const links = [
   {
     label: 'TikTok',
     text: 'Contenido corto sobre bolsa e inversiones',
-    href: 'https://www.tiktok.com/@bydanicash?_r=1&_t=ZS-96EKiEFqmym',
+    href: 'https://www.tiktok.com/@dani.cash90?_r=1&_t=ZS-96ld1SgrQcp',
     tag: 'Videos',
   },
   {
